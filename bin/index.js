9f60d85217421a5445a0030a855c2905a553b15d
sidneisimeao <sidneisimmon@gmail.com>
2018-09-16 20:04:25 -0300

    first commit

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+var arguments = process.argv.splice(2, process.argv.length - 1);
+
+
+console.log(arguments);
+//import readMarkDown from "../lib/readMarkDown";
+
+//readMarkDown('./');
+
+/*
+process.argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+  });
+
+*/

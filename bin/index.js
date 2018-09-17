951c526463a95b9f6c5b265ba51eb14db3d71e3d
Sidnei Simmon <sidneisimmon@gmail.com>
2018-09-17 11:10:35 -0300

    Teste

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,37 @@
 #! /usr/bin/env node
-var arguments = process.argv.splice(2, process.argv.length - 1);
+"use strict";
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-console.log(arguments);
-//import readMarkDown from "../lib/readMarkDown";
+var _fs = require("fs");
 
-//readMarkDown('./');
+var _fs2 = _interopRequireDefault(_fs);
 
-/*
-process.argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-  });
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-*/
+var argvs = process.argv.splice(2, process.argv.length - 1);
+
+var validateArgs = function validateArgs(argvs) {
+    if (argvs.length < 2) {
+        console.error("It is expected [path] [destiny]. Received" + argvs.join(' '));
+        process.exit(-1);
+    }
+    return argvs;
+};
+
+var _argvs = _slicedToArray(argvs, 2),
+    path = _argvs[0],
+    destiny = _argvs[1];
+
+_fs2.default.readdir(path, function (err, items) {
+    items.forEach(function (item) {
+
+        var file = path + '/' + item;
+
+        _fs2.default.readFile(file, 'utf-8', function (err, data) {
+            if (err) throw err;
+            console.log(data);
+        });
+    });
+});
+//# sourceMappingURL=index.js.map
---
+++ markdown/post.markdown
@@ -0,0 +1,40 @@
+# Título nível 1
+## Título nível 2
+### Título nível 3
+#### Título nível 4
+##### Título nível 5
+###### Título nível 6
+
+Javascript é _cool_!
+**Da2k** é a pronúncia para **Daciuk**: DA-TWO-K!
+
+[Blog do Da2k](https://blog.da2k.com.br)
+
+[Blog do Da2k](https://blog.da2k.com.br "Clique e acesse agora!")
+
+<https://www.google.com.br>
+
+> Esse é um bloco de citação.
+> Ele pode ter várias linhas por parágrafo.
+>
+> Inclusive, dando um espaço, você tem um novo parágrafo.
+
+* Item 1
+* Item 2
+* Item 3
+  
++ Item 1
++ Item 2
++ Item 3
+  
+- Item 1
+- Item 2
+- Item 3
+
+1. Item 1
+2. Item 2
+3. Item 3
+
+![Banana](http://cdn.osxdaily.com/wp-content/uploads/2013/07/dancing-banana.gif)
+
+`<blockquote>` é uma tag HTML.
